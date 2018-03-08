e3f7f8bdec5ee174db2900f9224300b1d74ebfcb
paweldomas <pawel.domas@jitsi.org>
2018-03-08 14:34:13 -0600

    squash with config move

--- react/features/base/config/actions.js
+++ react/features/base/config/actions.js
@@ -1,10 +1,13 @@
 /* @flow */
 
+import type { Dispatch } from 'redux';
+
 import {
     CONFIG_WILL_LOAD,
     LOAD_CONFIG_ERROR,
     SET_CONFIG
 } from './actionTypes';
+import { setConfigFromURLParams } from './index';
 
 /**
  * Signals that the configuration for a specific locationURL will be loaded now.
@@ -57,8 +60,33 @@ export function loadConfigError(error: Error, locationURL: string | URL) {
  * }}
  */
 export function setConfig(config: Object = {}) {
-    return {
-        type: SET_CONFIG,
-        config
+
+    return (dispatch: Dispatch<*>, getState: Function) => {
+        const { locationURL } = getState()['features/base/connection'];
+
+        // Now that the loading of the config was successful override the values
+        // with the parameters passed in the hash part of the location URI.
+        // TODO We're still in the middle ground between old Web with config,
+        // interfaceConfig, and loggingConfig used via global variables and new
+        // Web and mobile reading the respective values from the redux store.
+        // On React Native there's no interfaceConfig at all yet and
+        // loggingConfig is not loaded but there's a default value in the redux
+        // store.
+        // Only the config will be overridden on React Native, as the other
+        // globals will be undefined here. It's intentional - we do not care
+        // to override those configs yet.
+        locationURL && setConfigFromURLParams(
+
+            // On Web the config also comes from the window.config global,
+            // but it is resolved in the load config procedure.
+            config,
+            window && window.interfaceConfig,
+            window && window.loggingConfig,
+            locationURL);
+
+        dispatch({
+            type: SET_CONFIG,
+            config
+        });
     };
 }
